8d3bd76a27fdaee0ca6bf6eb0f6461d67080728f
Yuci Gou <yuci.gou@gmail.com>
2019-12-13 14:44:35 +0000

    Prometheus collects default metrics

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const Prometheus = require("prom-client");
+const express = require('express');
+const Prometheus = require('prom-client');
 
 const port = process.env.PORT || 3030;
 const app = express();
 
+Prometheus.collectDefaultMetrics();
+
 const hostLiveStatus = new Prometheus.Gauge({
-  name: "xpub_epmc_host_live_status",
-  help: "Situation where xpub-epmc host is running: 1 means OK, 0 means Error",
-  labelNames: ["status"]
+  name: 'xpub_epmc_host_live_status',
+  help: 'Situation where xpub-epmc host is running: 1 means OK, 0 means Error',
+  labelNames: ['status']
 });
 
-app.get("/metrics", (req, res) => {
+app.get('/metrics', (req, res) => {
   hostLiveStatus.set(1);
-  res.set("Content-Type", Prometheus.register.contentType);
+  res.set('Content-Type', Prometheus.register.contentType);
   res.end(Prometheus.register.metrics());
 });
 
